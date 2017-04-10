7aa1fe90bb976c5b95f6120b2ab67097e046c260
Dalex Tanguy <dalex.tanguy@gmail.com>
2017-04-10 11:30:33 +0200

    add empty BDD + add new theme

---
+++ wp-content/themes/clubcritiques/README.txt
@@ -0,0 +1,11 @@
+=== Club des critiques ===
+
+Contributors: Romain Richard, Alexandre Bornstein, Tanguy Dalex
+at least: WordPress 4.7
+
+Tested up to: WordPress 4.7
+
+Version: 1.0
+
+License: GPLv2 or later
+
---
+++ wp-content/themes/clubcritiques/template-parts/navigation/navigation-top.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php _e( 'Top Menu', 'clubcritiques' ); ?>">
+	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php echo clubcritiques_get_svg( array( 'icon' => 'bars' ) ); echo clubcritiques_get_svg( array( 'icon' => 'close' ) ); _e( 'Menu', 'clubcritiques' ); ?></button>
+	<?php wp_nav_menu( array(
+		'theme_location' => 'top',
+		'menu_id'        => 'top-menu',
+	) ); ?>
+
+	<?php if ( ( clubcritiques_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
+		<a href="#content" class="menu-scroll-down"><?php echo clubcritiques_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'clubcritiques' ); ?></span></a>
+	<?php endif; ?>
+</nav><!-- #site-navigation -->
