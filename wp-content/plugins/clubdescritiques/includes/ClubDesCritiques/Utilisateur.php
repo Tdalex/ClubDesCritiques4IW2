d0653ad05863ae94bde2db0a09df142397cabb35
Dalex Tanguy <dalex.tanguy@gmail.com>
2017-06-26 18:03:49 +0200

    update user info + mail change email/password

--- wp-content/plugins/clubdescritiques/includes/ClubDesCritiques/Utilisateur.php
+++ wp-content/plugins/clubdescritiques/includes/ClubDesCritiques/Utilisateur.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * Club Des Critiques Extension
  *
@@ -55,6 +55,7 @@ class Utilisateur{
 				wp_set_current_user($user->ID);
 				wp_set_auth_cookie($user->ID);
 				do_action( 'wp_login', $user->user_login );
+				
 				if(!get_field('activated', 'user_'.$user->ID)){
 					$activate = "
 					<form action='' method='POST'>
@@ -102,12 +103,12 @@ class Utilisateur{
 		
 		$user = wp_get_current_user();
 		wp_set_password($request['newPassword'], $user->ID);
-		update_field('activated', true, 'user_'.$user->ID);
+		update_field('activated', 'a:1:{i:0;s:4:"true";}', 'user_'.$user->ID);
 		$object    = 'Activation du compte terminé';
 		$message   = 'Bonjour,<br><br>Votre compte a bien été activé,<br> en esperant vous voir tres bientôt<br> Cordialement, <br><BR> Le club des critiques';
 		$headers[] = 'From: '. NO_REPLY;
 		wp_mail($user->user_email , $object, $message, $headers);	
-		$_SESSION['login_msg'] = 'Votre compte a bien été activé'
+		$_SESSION['login_msg'] = 'Votre compte a bien été activé';
 		wp_set_current_user($user->ID);
 		wp_set_auth_cookie($user->ID);
 		do_action( 'wp_login', $user->user_login );
@@ -117,14 +118,36 @@ class Utilisateur{
 	}
 	
 	public static function modifyUserInfo($request){
-		$user_id = get_current_user_id();
-		$request['userInfo']['ID'] = $user_id;
-		
+		$user = wp_get_current_user();
+		$request['ID'] = $user->ID;
+		$lastEmail = $user->user_email;
 		//change password
-		if(!empty($request['newPassword']) && $request['newPassword'] == $request['newPasswordCheck'])
-			wp_set_password($newPassword, $user_id);
-			
-		return wp_update_user($request['userInfo']);
+		if(!empty($request['password'])){
+			if(strlen($request['password'])>=6){
+				if($request['password'] == $request['passwordCheck']){
+					wp_set_password($request['password'], $user_id);
+					$object    = 'Modification de votre mot de passe';
+					$message   = 'Bonjour,<br><br>Votre mot de passe vient d\'êtres modifié,<br> En cas de problème veuillez nous contacter au plus tôt, sinon veuillez ignorer ce message.<br> Cordialement, <br><BR> Le club des critiques';
+					$headers[] = 'From: '. NO_REPLY;
+					wp_mail($user->user_email , $object, $message, $headers);	
+				}else{
+					echo 'Veuillez valider la vérification de mot de passe';
+				}
+			}else{
+				echo 'Veuillez entrer un mot de passe plus long';	
+			}
+		}
+		unset($request['password']);
+		unset($request['passwordCheck']);
+		$request = array_filter($request);
+		
+		if($lastEmail != $request['email']){
+			$object    = 'Modification de votre email';
+			$message   = 'Bonjour,<br><br>Votre email vient d\'êtres modifié, En cas de problème veuillez nous contacter au plus tôt,<br> sinon veuillez ignorer ce message.<br> Cordialement, <br><BR> Le club des critiques';
+			$headers[] = 'From: '. NO_REPLY;
+			wp_mail($lastEmail , $object, $message, $headers);
+		}
+		return wp_update_user($request);
 	}
 	
 	public static function postComment($idProduct, $content){
--- wp-content/themes/clubcritiques/template-parts/MyProfil.php
+++ wp-content/themes/clubcritiques/template-parts/MyProfil.php
@@ -7,16 +7,19 @@ use ClubDesCritiques\Utilisateur as Utilisateur;
 
 $user = wp_get_current_user();
 
+if(isset($_POST)){
+	Utilisateur::modifyUserInfo($_POST);
+}
 ?>
 
 
 <form action="" method="POST">
-	Email: <input type='text' name='email' value='<?php echo $user->user_email;?>'></input><br>
+	Email: <input type='text' name='user_email' value='<?php echo $user->user_email;?>'></input><br>
 	Mot de passe: <input type='password' name='password'></input><br>
-	Vérification mot de passe: <input type='password' name='newPasswordCheck'></input><br>
+	Vérification mot de passe: <input type='password' name='passwordCheck'></input><br>
 	<br>
-	Prénom: <input type='text' name='firstname' value='<?php echo $user->firstname;?>'></input><br>
-	Prénom: <input type='text' name='lastname' value='<?php echo $user->lastname;?>'></input><br>
-	
+	Prénom: <input type='text' name='first_name' value='<?php echo $user->user_firstname;?>'></input><br>
+	Nom de famille: <input type='text' name='lastname' value='<?php echo $user->user_lastname ;?>'></input><br>
+	<br>
 	<button type='submit'>Modifier mes infos</button>
 </form>
