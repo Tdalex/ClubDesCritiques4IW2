20c9d7d6638932313c31741df63bff7ed63cdd9e
romainrichard08 <romain.richard.08@hotmail.fr>
2017-07-11 22:45:52 +0200

    modif front author & single product

--- wp-content/themes/clubcritiques/template-parts/Author.php
+++ wp-content/themes/clubcritiques/template-parts/Author.php
@@ -26,18 +26,81 @@ $birthdate = get_field('birthdate', $author_ID);
 $birthdate = date($birthdate);
 $deathdate = get_field('deathdate', $author_ID);
 $deathdate = date($deathdate);
+
+get_header(); 
 ?>
 
-<div><?php echo $author->post_title;?></div>
-<div><?php echo $birthdate. ' --- ' . $deathdate?></div>
-<img style='height:50px; length:50px;'src='<?php echo $photo;?>'></img>
-<div><?php echo $descriptionAuthor;?></div>
-<div><?php echo $sexe;?></div>
-<div><?php echo $description;?></div>
+<div class="container contain">
 
-<p>Bibliographie</p>
+	<div class="row">
+		<div class="col-md-3">
+			<?php if(!$image){ ?> 
+				<img class="img-responsive img-livre" style='height:300px; length:300px;' src="https://pictures.abebooks.com/isbn/9782070543588-fr.jpg"> 
+			<?php }else{ ?>
+				<img class="img-responsive img-livre" style='height:300px; length:300px;' src="<?php echo $photo; ?>"></img>
+			<?php } ?>
+		</div>
+
+		<div class="col-md-9">
+			<div class="row">
+				<h1 class="title"><?php echo $author->post_title;?></h1>
+			</div>
+		</div>
+
+		<div class="col-md-9">
+			<div class="row author interligne_big">
+				<?php echo $birthdate. ' - ' . $deathdate?>
+			</div>
+		</div>
+
+		<div class="col-md-9">
+			<div class="row description interligne_small">
+				<?php echo $descriptionAuthor;?>
+			</div>
+		</div>
+
+		<div><?php echo $description;?></div>
+	</div>
+
+	<div class="row">
+		<div class="row">
+			<h2 class="cat_h2">Bibliographie</h2>
+		</div>
+		<div class="row book_author">
+			<ul>
+				<?php
+				foreach(Bibliotheque::getAuthorBiblio($author_ID) as $biblio)
+				{ ?>
+				<li class="row single_book col-md-9">
+
+					<div class="col-md-3">
+						<a href='<?php echo get_permalink(get_page_by_title('Produit')).$biblio->ID; ?>'>
+							<?php if(!$image){ ?> 
+								<img class="img-responsive img-livre" src="https://pictures.abebooks.com/isbn/9782070543588-fr.jpg"> 
+							<?php }else{ ?>
+								<img class="img-responsive img-livre" src="<?php echo get_field('image', $sp->ID); ?>"></img>
+							<?php } ?>
+					</div>
+					<div class="col-md-9">
+							<div class="row">
+								<h3><?php echo $biblio->post_title;?></h3>	
+							</div>
+						</a>
+							<div class="row description">
+							<p>
+								[description] Retrace les événement et les coulisses de l'Anschluss lorsque la Wehrmacht entre triomphalement en Autriche et s'interroge sur les fondements des premiers... <a href='<?php echo get_permalink(get_page_by_title('Produit')).$biblio->ID; ?>'>[lire la suite]</a>
+							</p>
+							</div>
+					</div>
+				</li>
+				<?php } ?>
+			</ul>
+
+	</div>
+
+</div>
+
 <?php
-foreach(Bibliotheque::getAuthorBiblio($author_ID) as $biblio){ ?>
-	<a href='<?php echo get_permalink(get_page_by_title('Produit')).$biblio->ID; ?>'><?php echo $biblio->post_title;?></a><br>
-<?php }
+get_footer(); 
+
 ?>
--- wp-content/themes/clubcritiques/template-parts/SingleProduct.php
+++ wp-content/themes/clubcritiques/template-parts/SingleProduct.php
@@ -84,144 +84,153 @@ if(count($suggestedProductsTemp)<4){
 get_header(); 
 ?>
 
-<div class="container">
-	<div class="row">
-	<?php if(isset($_SESSION['message'])){ ?>
-		<div class="alert alert-<?php echo $_SESSION['message']['type'] ?>">
-		  <?php echo $_SESSION['message']['text']; ?>
-		</div>	
-	<?php unset($_SESSION['message']);
-		} ?>
-		<div class="col-md-3">
-		<?php if(!$image){ ?> 
-			<img class="img-responsive img-livre" style='height:300px; length:300px;' src="https://pictures.abebooks.com/isbn/9782070543588-fr.jpg"> 
-		<?php }else{ ?>
-			<img class="img-responsive img-livre" style='height:300px; length:300px;' src="<?php echo $image; ?>"></img>
+<div class="container-fluid contain">
+	<div class="row header_page">
+		<div class="container">
+			<?php if(isset($_SESSION['message'])){ ?>
+				<div class="alert alert-<?php echo $_SESSION['message']['type'] ?>">
+				  <?php echo $_SESSION['message']['text']; ?>
+				</div>	
+			<?php unset($_SESSION['message']);
+				} ?>
+				<div class="col-md-3">
+				<div class="row">
+					<?php if(!$image){ ?> 
+						<img class="img-responsive img-livre" src="https://pictures.abebooks.com/isbn/9782070543588-fr.jpg"> 
+					<?php }else{ ?>
+						<img class="img-responsive img-livre" src="<?php echo $image; ?>"></img>
+					<?php } ?>
+				</div>
+				
+				<div class="row btn-echanges">
+				<?php if(is_user_logged_in()){ ?>
+						<div class="col-md-12">
+							<form method='POST' action=''>
+								<button type='submit' class="btn" value='give' name='type'>Donner</button>
+								<button type='submit' class="btn" value='take' name='type'>Recevoir</button>
+								<button type='submit' class="btn" value='deleteExchange' name='type'>Annuler</button>
+							</form>
+						</div>
+					<?php } ?>
+					</div>
+				</div>
+				
+				<div class="col-md-9">
+					<div class="row">
+						<h1 class="title"><?php echo $product->post_title; ?></h1>
+					</div>
+
+					<div class="row author">		
+						<a href='<?php echo get_permalink(get_page_by_title('Auteur')).$author->ID; ?>'><?php echo $author->post_title;?></a>
+						<?php echo $published_date; ?>
+					</div>
+
+					<div class="row">
+						<div class="rating"><!--
+						   --><a href="#5" title="Donner 5 étoiles">★</a><!--
+						   --><a href="#4" title="Donner 4 étoiles">★</a><!--
+						   --><a href="#3" title="Donner 3 étoiles">★</a><!--
+						   --><a href="#2" title="Donner 2 étoiles">★</a><!--
+						   --><a href="#1" title="Donner 1 étoile">★</a>
+						</div>
+						<span><?php echo $averageNote['total']; ?> notations</span>
+					</div>
+
+					<div class="row description">
+					<?php echo $description; ?>
+					</div>
+				</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<h2 class="cat_h2">Autres suggestions</h2>
+		</div>
+
+		<div class="row">
+		<?php foreach($suggestedProductsTemp as $sp){ ?>
+			<div class="col-md-3 suggestions col-xs-6">
+				<a href="<?php echo get_permalink(get_page_by_title('Produit')).$sp->ID; ?>">
+					<?php if(!get_field('image', $sp->ID)){ ?> 
+						<img class="img-responsive"  src="https://pictures.abebooks.com/isbn/9782070543588-fr.jpg"> 
+					<?php }else{ ?>
+						<img class="img-responsive"  src="<?php echo get_field('image', $sp->ID); ?>"></img>
+					<?php } ?>
+					<h3><?php echo $sp->post_title; ?></h3>
+					<p><?php echo get_field('author', $sp->ID)[0]->post_title; ?></p>
+				</a>
+			</div>
 		<?php } ?>
 		</div>
 		
-		<div class="col-md-9">
-			<div class="row">
-				<h1 class="title"><?php echo $product->post_title; ?></h1>
-			</div>
-
-			<div class="row author">		
-				<a href='<?php echo get_permalink(get_page_by_title('Auteur')).$author->ID; ?>'><?php echo $author->post_title;?></a>
-				<?php echo $published_date; ?>
-			</div>
-
-			<div class="row">
-				<div class="rating"><!--
-				   --><a href="#5" title="Donner 5 étoiles">★</a><!--
-				   --><a href="#4" title="Donner 4 étoiles">★</a><!--
-				   --><a href="#3" title="Donner 3 étoiles">★</a><!--
-				   --><a href="#2" title="Donner 2 étoiles">★</a><!--
-				   --><a href="#1" title="Donner 1 étoile">★</a>
-				</div>
-				<span><?php echo $averageNote['total']; ?> notations</span>
-			</div>
-
-			<div class="row description">
-			<?php echo $description; ?>
-			</div>
-		</div>
-				<?php if(is_user_logged_in()){ ?>
-			<div class="col-md-3">
-				<form method='POST' action=''>
-					<button type='submit' class="btn" value='give' name='type'>Donner</button>
-					<button type='submit' class="btn" value='take' name='type'>Recevoir</button>
-					<button type='submit' class="btn" value='deleteExchange' name='type'>Annuler</button>
-				</form>
-			</div>
-		<?php } ?>
-	</div>
-
-	<div class="row">
-		<h2 class="cat_h2">Autres suggestions</h2>
-	</div>
-
-	<div class="row">
-	<?php foreach($suggestedProductsTemp as $sp){ ?>
-		<div class="col-md-3 suggestions col-xs-6">
-			<a href="<?php echo get_permalink(get_page_by_title('Produit')).$sp->ID; ?>">
-				<?php if(!get_field('image', $sp->ID)){ ?> 
-					<img class="img-responsive"  src="https://pictures.abebooks.com/isbn/9782070543588-fr.jpg"> 
-				<?php }else{ ?>
-					<img class="img-responsive"  src="<?php echo get_field('image', $sp->ID); ?>"></img>
-				<?php } ?>
-				<h3><?php echo $sp->post_title; ?></h3>
-				<p><?php echo get_field('author', $sp->ID)[0]->post_title; ?></p>
-			</a>
-		</div>
-	<?php } ?>
-	</div>
-	
-	<?php if(is_user_logged_in()){ ?>
-	
-	<div class="row">
-		<form method='POST' action=''>
-			Note: <select name='userNote' id="userNote">
-				<?php for($i=0;$i<=5;$i++){ ?>
-					<?php if($i==$userNote){ ?>
-						<option selected value="<?php echo $i; ?>"><?php echo $i; ?></option> 
-					<?php }else{ ?>
-						<option value="<?php echo $i; ?>"><?php echo $i; ?></option> 
+		<?php if(is_user_logged_in()){ ?>
+		
+		<div class="row">
+			<form method='POST' action=''>
+				Note: <select name='userNote' id="userNote">
+					<?php for($i=0;$i<=5;$i++){ ?>
+						<?php if($i==$userNote){ ?>
+							<option selected value="<?php echo $i; ?>"><?php echo $i; ?></option> 
+						<?php }else{ ?>
+							<option value="<?php echo $i; ?>"><?php echo $i; ?></option> 
+						<?php } ?>
 					<?php } ?>
+				</select>/5<br>
+				Commentaire: 
+				<?php if( Utilisateur::getUserComment($product_ID)){
+				$userComment = Utilisateur::getUserComment($product_ID); ?>
+				<textarea name='comment'><?php echo strip_tags($userComment->post_content); ?></textarea>
+				<button type='submit' value='deleteComment' name='type'>supprimer</button>
+				<?php }else{ ?>
+				<textarea name='comment'></textarea>
 				<?php } ?>
-			</select>/5<br>
-			Commentaire: 
-			<?php if( Utilisateur::getUserComment($product_ID)){
-			$userComment = Utilisateur::getUserComment($product_ID); ?>
-			<textarea name='comment'><?php echo strip_tags($userComment->post_content); ?></textarea>
-			<button type='submit' value='deleteComment' name='type'>supprimer</button>
-			<?php }else{ ?>
-			<textarea name='comment'></textarea>
-			<?php } ?>
-			<button type='submit' value='comment' name='type'>commenter</button>
-			
-		</form>
-	</div>
-	<?php } ?>
-	<br>
-	<h2>Commentaires</h2>
-	<div class="row">
-		<?php foreach($comments as $comment){ ?>
-			<?php $commentAuthor = get_user_by('ID', $comment->post_author); ?>
-			<div class="row">
-				<div class="col-md-3 comments col-xs-6">
-					<h2><a href='<?php echo get_permalink(get_page_by_title('utilisateur')).$commentAuthor->ID; ?>'><?php echo $commentAuthor->user_firstname .' '. $commentAuthor->user_lastname; ?></a> <span><?php echo Utilisateur::getNotation($product_ID, $commentAuthor->ID); ?>/5</span></h2>
-					<?php  echo $comment->post_content; ?>
-				</div>
-			</div><br>
+				<button type='submit' value='comment' name='type'>commenter</button>
+				
+			</form>
+		</div>
 		<?php } ?>
+		<br>
+		<h2>Commentaires</h2>
+		<div class="row">
+			<?php foreach($comments as $comment){ ?>
+				<?php $commentAuthor = get_user_by('ID', $comment->post_author); ?>
+				<div class="row">
+					<div class="col-md-3 comments col-xs-6">
+						<h2><a href='<?php echo get_permalink(get_page_by_title('utilisateur')).$commentAuthor->ID; ?>'><?php echo $commentAuthor->user_firstname .' '. $commentAuthor->user_lastname; ?></a> <span><?php echo Utilisateur::getNotation($product_ID, $commentAuthor->ID); ?>/5</span></h2>
+						<?php  echo $comment->post_content; ?>
+					</div>
+				</div><br>
+			<?php } ?>
+		</div>
+		
+		
+		<br>
+		<?php if (isset($exchanges['give']) || isset($exchanges['take'])){ ?>
+		<h2>Ces personnes souhaitent echanger</h2>
+		<?php if(isset($exchanges['take'])){ ?>
+			<h3>Recevoir</h3>
+			<div class="row">
+				<?php foreach($exchanges['take'] as $exchange){ ?>
+					<?php $exchangeAuthor = get_user_by('ID', $exchange->post_author); ?>
+					<div class="col-md-3 comments col-xs-6">
+						<h2><a href='<?php echo get_permalink(get_page_by_title('utilisateur')).$exchangeAuthor->ID; ?>'><?php echo $exchangeAuthor->user_firstname .' '. $exchangeAuthor->user_lastname; ?></a></h2>
+					</div>
+				<?php } ?>
+			</div>
+		<?php }?>
+		<?php if (isset($exchanges['give'])){ ?>
+			<h3>Donner</h3>
+			<div class="row">
+				<?php foreach($exchanges['give'] as $exchange){ ?>
+					<?php $exchangeAuthor = get_user_by('ID', $exchange->post_author); ?>
+					<div class="col-md-3 comments col-xs-6">
+						<h2><a href='<?php echo get_permalink(get_page_by_title('utilisateur')).$exchangeAuthor->ID; ?>'><?php echo $exchangeAuthor->user_firstname .' '. $exchangeAuthor->user_lastname; ?></a></h2>
+					</div>
+				<?php } ?>
+			</div>
+		<?php }}?>
 	</div>
-	
-	
-	<br>
-	<?php if (isset($exchanges['give']) || isset($exchanges['take'])){ ?>
-	<h2>Ces personnes souhaitent echanger</h2>
-	<?php if(isset($exchanges['take'])){ ?>
-		<h3>Recevoir</h3>
-		<div class="row">
-			<?php foreach($exchanges['take'] as $exchange){ ?>
-				<?php $exchangeAuthor = get_user_by('ID', $exchange->post_author); ?>
-				<div class="col-md-3 comments col-xs-6">
-					<h2><a href='<?php echo get_permalink(get_page_by_title('utilisateur')).$exchangeAuthor->ID; ?>'><?php echo $exchangeAuthor->user_firstname .' '. $exchangeAuthor->user_lastname; ?></a></h2>
-				</div>
-			<?php } ?>
-		</div>
-	<?php }?>
-	<?php if (isset($exchanges['give'])){ ?>
-		<h3>Donner</h3>
-		<div class="row">
-			<?php foreach($exchanges['give'] as $exchange){ ?>
-				<?php $exchangeAuthor = get_user_by('ID', $exchange->post_author); ?>
-				<div class="col-md-3 comments col-xs-6">
-					<h2><a href='<?php echo get_permalink(get_page_by_title('utilisateur')).$exchangeAuthor->ID; ?>'><?php echo $exchangeAuthor->user_firstname .' '. $exchangeAuthor->user_lastname; ?></a></h2>
-				</div>
-			<?php } ?>
-		</div>
-	<?php }}?>
 </div>
 
 <?php
